7c72614b73194d15c359fd332cf384e4cf411d20
ErebosXYZ <robertmaynou@gmail.com>
2025-04-09 19:31:51 +0200

    done

--- action.js
+++ action.js
@@ -1,7 +1,7 @@
 //for implementation details visit marinsborg.com
 var apiUrl = "https://pokeapi.co/api/v2/pokemon/?"; //API base URL
 var offset = 0;
-var limit = 10; //limiting Pokemons - sprites are not numbered properly after 665
+var limit = 600; //limiting Pokemons - sprites are not numbered properly after 665
 var pokemonUrl = apiUrl + "limit=" + limit + "&offset=" + offset; //complete URL with limit
 var spriteUrl =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"; //base URL from which sprites are fetched
@@ -12,6 +12,7 @@ const pokemonNameElement = document.getElementById("pokemon-name"); //element ob
 var streak = 0; //initialize streak to zero
 var pokemonName = ""; //set pokemon name as global variable
 var pokemonData; //variable which holds the response from Pokemon API
+const button = document.getElementById("button");
 
 //add event listener on input field and when user presses Enter key, execute the function
 guess.addEventListener("keypress", function (e) {
@@ -19,6 +20,10 @@ guess.addEventListener("keypress", function (e) {
     checkGuess();
   }
 });
+
+guess.addEventListener("click", playPokemonMp3);
+
+button.addEventListener("click", checkGuess);
 
 // Execute this function to play the audio
 function playPokemonMp3() {
@@ -40,7 +45,7 @@ var main = async function () {
 
 //function that compares player's guess with Pokemon name and based on that either increases or resets streak
 function checkGuess() {
-  if (pokemonName.toLowerCase() === guess.value) {
+  if (pokemonName.toLowerCase() === guess.value.toLowerCase()) {
     streak++; //correct guess - increase streak by one
   } else {
     streak = 0; //wrong guess - reset streak
